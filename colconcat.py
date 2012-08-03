a56a2ec00fd072da348df61f89a7516ed1133631
Varun Singh <varun.singh@gmail.com>
2012-08-03 23:19:58 +0300

    initial commit

---
+++ colconcat.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import sys
+import csv
+    
+def main(argv):
+    fs = []
+    fn = len(argv)
+    '''
+    if last param is output file then uncomment the 2 lines below
+    and dont read that file.
+    '''
+    #newname = argv[fn-1]
+    #fn =fn-1
+    fw = open('output.txt', 'w')
+    num_lines = sum(1 for line in open(argv[0]))
+    try:
+        i =0
+        for cli in argv:
+            fs.append(open(argv[i], 'rb'))
+            i=i+1
+        for line in xrange(num_lines):
+            for j in xrange(fn):
+                fw.write(fs[j].readline().rstrip('\r\n'))
+                if (j<fn-1):
+                    fw.write(" ")
+                else:
+                    fw.write("\n")
+    finally:
+        for f in fs:
+            f.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
