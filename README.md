c0500a2c0ba07a7f78a5751d4524bb5521bb8d9f
vr000m <varun.singh@gmail.com>
2013-04-20 02:51:53 +0300

    list acronyms in tex files
    Script helps me find the acronyms in my PhD Thesis and compares them to
    the ones already listed in list_of_abbreviations.tex

--- README.md
+++ README.md
@@ -4,8 +4,15 @@
 * `markov_loss.py`: Simple function to generate loss patterns based on a
   two-state markov chain. `markov_loss(p,q)`
 * `perinst.awk`: averaging time series data into **1s intervals**
-* `\gnuplot\`: Scripts to create plots (stacked multiplot, quad plots, stacked curves, etc). 
-  More at [Gnuplot Demo](http://gnuplot.sourceforge.net/demo_cvs/)
-* `gstreamer.cli`: [Gstreamer](http://gstreamer.freedesktop.org/) command lines
-* `tcptrace.cli`: [tcpdump](http://www.tcpdump.org/) to [tcptrace](http://www.tcptrace.org/) 
-  to [gnuplot](http://www.gnuplot.info/) toolchain
+* `\gnuplot\`: Scripts to create plots (stacked multiplot, quad plots, stacked
+  curves, etc). More at [Gnuplot Demo](http://gnuplot.sourceforge.net/demo_cvs/)
+* `gstreamer.cli`: [Gstreamer](http://gstreamer.freedesktop.org/) command
+  lines
+* `tcptrace.cli`: [tcpdump](http://www.tcpdump.org/) to
+  [tcptrace](http://www.tcptrace.org/) to [gnuplot](http://www.gnuplot.info/)
+  toolchain
+* `\dummynet\`: Scripts to run automate dummynet simulations (based on input
+  from @marcinag)
+* `laf.py`: Simple script that goes through my thesis .tex files and looks for
+  acronyms. It stores the acronyms in the `tex files` and compares with those
+  already listed in `list.tex` file.
---
+++ laf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# list acronyms in file (*.tex)
+import sys
+import os
+import subprocess
+import re
+
+camel_case = re.compile('(?:[A-Z])(?:[a-z])+(?:[A-Z])+') #e.g., WebRTC, QoS, QoE
+only_word=re.compile("[^\w']")	# ignore alphaneumeric
+parantheses = re.compile("\((.*)\)") # (words)
+
+def print_list(_list):
+	for t in _list:
+		print t
+
+def main(args):
+	findCommand = 'find . -type f -iregex \'.*\.tex\' -print'
+	print "Command:", findCommand
+	texFiles = subprocess.check_output(findCommand, shell=True).split()
+	# print texFiles
+	# texFiles = filter(None, texFiles)
+	inList = []
+	newList = []
+	for t in texFiles:
+		print "Reading File:", t
+		f = file(t).read()
+		for word in f.split():
+			
+			# In latex some words are immediately followed by ~
+			# Typically for \cite{} or \ref{}
+			if (word.find('~')!=-1):
+				w = word.split('~')
+				# just keeping the first part of the word, 
+				# everything after ~ is prolly a latex command
+				word=w[0]
+				# print w
+			
+			# Fetching words inside (parantheses)
+			res = parantheses.search( word )
+			if(res!=None):
+				word=res.group(1)
+				# print "parantheses", word
+			
+			# check if Word is uppercase
+			# check CamelCase
+			if (word.isupper() or camel_case.match(word)!=None):
+				accr=only_word.sub('', word)
+				if(len(accr)>1): 
+					# create list of acronyms already in list
+					# create list of acronyms in 
+					if(t.find('lists')!=-1): #t=="_0lists.tex" 
+						if accr not in inList:
+						    inList.append(accr)
+					else:
+						if accr not in newList:
+						    newList.append(accr)
+	newList.sort()
+	# print "newList: ", newList
+	inList.sort()
+	# print "inList: ", inList
+	print "Consider adding these"
+	print_list(set(newList)-set(inList))
+	print "\nConsider removing these: (not in use):"
+	print_list(set(inList)-set(newList))
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
